c4d0097ef302c06497025cc84ef1c0035a5a4fe4
Edmondo Pentangelo <edmondo@gmail.com>
2021-09-19 20:53:10 +0100

    Refactor

--- Main.c
+++ Main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-enum Tag {Int, Float, Char};
-union Value { int v_1; float v_2; char v_3; };
+enum Tag {Int, Double, Char, String };
+union Value { int v_1; double v_2; char v_3; char* v_4; };
 struct BoxedValue {
     enum Tag tag;
     union Value value;
@@ -9,10 +9,10 @@ struct BoxedValue {
 
 struct closure {
    void* fn;
-   struct BoxedValue* env;
+   struct BoxedValue** env;
 };
 
-struct closure* mkClosure(void* f, struct BoxedValue* arg) {
+struct closure* mkClosure(void* f, struct BoxedValue** arg) {
    struct closure * c = (struct closure *)malloc(sizeof(struct closure));
    c->env = arg;
    c->fn = f;
@@ -36,12 +36,18 @@ struct BoxedValue* mkInt(int x) {
    return mkBoxedValue(Int, v);
 }
 
-struct BoxedValue* add(struct BoxedValue* x, struct BoxedValue* y) {
-   return mkInt(x->value.v_1 + y->value.v_1);
+struct BoxedValue* add(struct BoxedValue* env[], struct BoxedValue* y) {
+   return mkInt(env[0]->value.v_1 + y->value.v_1);
+}
+
+struct BoxedValue** singleton(struct BoxedValue* arg) {
+   static struct BoxedValue* args[1]; 
+   args[0] = arg;
+   return args;  
 }
 
 int main() {
-   struct closure * c = mkClosure(&add, mkInt(3));
+   struct closure * c = mkClosure(&add, singleton(mkInt(3)));
    struct BoxedValue* ret = applyClosure(c, mkInt(2));
    printf("result is %d\n", ret->value.v_1);
 }
