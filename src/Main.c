be6d3df58e2c09fa579bd2c2d6f27e3b8f285f42
Edmondo Pentangelo <edmondo@gmail.com>
2021-10-03 15:01:13 +0100

    Add sub and refactor header file

--- src/Main.c
+++ src/Main.c
@@ -7,14 +7,14 @@ void fromIntegerExample1() {
    struct BoxedValue* anf_0 = applyClosure(fromInteger, numInt);
    struct BoxedValue* anf_1 = mkInt(4);
    struct BoxedValue* anf_2 = applyClosure(anf_0, anf_1);
-   printf("%d\n", anf_2->value.v_1);
+   printf("fromInteger 4 : Int --> %d\n", anf_2->value.v_1);
 }
 
 void fromIntegerExample2() {
    struct BoxedValue* anf_0 = applyClosure(fromInteger, numDouble);
    struct BoxedValue* anf_1 = mkInt(4);
    struct BoxedValue* anf_2 = applyClosure(anf_0, anf_1);
-   printf("%f\n", anf_2->value.v_2);
+   printf("fromInteger 4 : Double --> %f\n", anf_2->value.v_2);
 }
 
 void plusExample() {
@@ -27,7 +27,7 @@ void plusExample() {
    struct BoxedValue* anf_5 = mkInt(2);
    struct BoxedValue* anf_7 = applyClosure(anf_4, anf_5);
    struct BoxedValue* anf_8 = applyClosure(anf_6, anf_7);
-   printf("%d\n", anf_8->value.v_1);
+   printf("1 + 2 --> %d\n", anf_8->value.v_1);
 }
 
 void mulExample() {
@@ -40,7 +40,7 @@ void mulExample() {
    struct BoxedValue* anf_5 = mkInt(2);
    struct BoxedValue* anf_7 = applyClosure(anf_4, anf_5);
    struct BoxedValue* anf_8 = applyClosure(anf_6, anf_7);
-   printf("%d\n", anf_8->value.v_1);
+   printf("2 * 3 --> %d\n", anf_8->value.v_1);
 }
 
 void eqeqExample() {
@@ -53,7 +53,20 @@ void eqeqExample() {
    struct BoxedValue* anf_5 = mkInt(3);
    struct BoxedValue* anf_7 = applyClosure(anf_4, anf_5);
    struct BoxedValue* anf_8 = applyClosure(anf_6, anf_7);
-   printf("%s\n", (anf_8->value.v_1) ? "True" : "False");
+   printf("2 == 3 --> %s\n", (anf_8->value.v_1) ? "True" : "False");
+}
+
+void subExample() {
+   struct BoxedValue* anf_2 = applyClosure(__sub, numInt);
+   struct BoxedValue* anf_0 = applyClosure(fromInteger, numInt);
+   struct BoxedValue* anf_1 = mkInt(11);
+   struct BoxedValue* anf_3 = applyClosure(anf_0, anf_1);
+   struct BoxedValue* anf_6 = applyClosure(anf_2, anf_3);
+   struct BoxedValue* anf_4 = applyClosure(fromInteger, numInt);
+   struct BoxedValue* anf_5 = mkInt(14);
+   struct BoxedValue* anf_7 = applyClosure(anf_4, anf_5);
+   struct BoxedValue* anf_8 = applyClosure(anf_6, anf_7);
+   printf("11 - 14 --> %d\n", anf_8->value.v_1);
 }
 
 int main() {
@@ -61,6 +74,7 @@ int main() {
    fromIntegerExample1();
    fromIntegerExample2();
    plusExample();
+   subExample();
    mulExample();
    eqeqExample();
    return 0;
